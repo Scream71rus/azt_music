a4063410c1b4d1f9b8fb8ab785bde622fdc7a566
Dima <scream71rus@mail.ru>
2019-05-01 20:09:22 +0300

    fix

--- handlers/ws.py
+++ handlers/ws.py
@@ -14,14 +14,18 @@ class WSMusicHandler(WebSocketHandler, BaseHandler):
 
     def open(self, *args):
         ws_users.append(self)
+        print(len(ws_users))
 
     def on_message(self, message):
+        print(message)
         if message == 'Go to start!':
             status_to_start.append(self)
             if len(ws_users) == len(status_to_start):
                 t = time.time()
-                start = int((t + 30) * 1000)
+                start = int((t + 10) * 1000)
                 self.write_message(str(start))
 
     def on_close(self):
         ws_users.remove(self)
+        status_to_start.remove(self)
+        print(len(ws_users))
