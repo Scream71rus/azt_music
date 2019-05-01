45bd0216f2159716e54c2a3ef5e5ec56069c7d7c
Dima <scream71rus@mail.ru>
2019-05-01 19:29:01 +0300

    added start to time

--- handlers/ws.py
+++ handlers/ws.py
@@ -1,20 +1,27 @@
 
+import datetime
+import time
+
 from tornado.websocket import WebSocketHandler
 
 from handlers.base_handler import BaseHandler
 
 ws_users = []
+status_to_start = []
 
 
 class WSMusicHandler(WebSocketHandler, BaseHandler):
 
     def open(self, *args):
         ws_users.append(self)
-        print(ws_users)
 
     def on_message(self, message):
-        pass
+        if message == 'Go to start!':
+            status_to_start.append(self)
+            if len(ws_users) == len(status_to_start):
+                t = time.time()
+                start = int((t + 60) * 1000)
+                self.write_message(str(start))
 
     def on_close(self):
-        print(1)
         ws_users.remove(self)
